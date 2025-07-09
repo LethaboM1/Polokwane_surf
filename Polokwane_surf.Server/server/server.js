43ac72dc6f111b7ba1e86a75724c4af2c7a805dd
LethaboM1 <developer@hillary.co.za>
2025-07-09 10:22:26 +0200

    server

---
+++ Polokwane_surf.Server/server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/api/hello', (req, res) => {
+  res.send({ message: 'Hello from the backend!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
