deff6754de94c7888440db4abcb5b8339f03f9f8
LethaboM1 <developer@hillary.co.za>
2025-07-09 11:12:41 +0200

    vite changes

--- Polokwane_surf.Server/server/server.js
+++ Polokwane_surf.Server/server/server.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const PORT = process.env.PORT || 5000
 
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 app.get('/api/hello', (req, res) => {
-  res.send({ message: 'Hello from the backend!' });
-});
+  res.json({ message: 'Hello from backend!' })
+})
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
